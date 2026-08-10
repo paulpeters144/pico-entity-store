117679aba7614727786db9bc1cd252c603ece354
Paul Peters <paul.peters144@gmail.com>
2026-08-10 07:49:56 -0500

    docs: improve readability of parent example
    - Break dense nested calls into clear sequential steps
    - Use descriptive variable names (axe_ref instead of a)
    - Add #[allow(dead_code)] to suppress warnings

--- examples/parent.rs
+++ examples/parent.rs
@@ -1,27 +1,30 @@
 use pico_entity_store::prelude::*;
 
 #[derive(Clone)]
+#[allow(dead_code)]
 struct Dwarf {
     name: String,
 }
 
 #[derive(Clone)]
+#[allow(dead_code)]
 struct Axe {
     kind: String,
 }
 
 fn main() {
     let store = EntityStore::new();
-    let a = store
-        .get_by_id::<Axe>(store.add(Axe { kind: "Battleaxe".into() }, &[]).unwrap().id())
-        .unwrap();
-    store.add(Dwarf { name: "Gimli".into() }, &children![a]).unwrap();
+
+    let axe = Axe { kind: "Battleaxe".into() };
+    let axe_ref = store.add(axe, &[]).unwrap();
+
+    let gimli = Dwarf { name: "Gimli".into() };
+    let axe = store.get_by_id::<Axe>(axe_ref.id()).unwrap();
+    store.add(gimli, &children![axe]).unwrap();
 
     let axe = store.first::<Axe>().unwrap();
+
     // prints: parent of Battleaxe: Gimli
-    println!(
-        "parent of {}: {}",
-        axe.kind,
-        store.get_by_id::<Dwarf>(store.parent(&axe).unwrap().id()).unwrap().name
-    );
+    let parent = store.get_by_id::<Dwarf>(store.parent(&axe).unwrap().id()).unwrap();
+    println!("parent of {}: {}", axe.kind, parent.name);
 }
